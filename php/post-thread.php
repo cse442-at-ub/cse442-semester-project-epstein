d4d783ab33dd8cd60adc618670f33d1ab8b1b828
Joey Dubill <jwdubill@buffalo.edu>
2020-03-22 11:07:30 -0400

    Fixed one last merge issue, removed redundant loginpage.html

--- php/post-thread.php
+++ php/post-thread.php
@@ -1,24 +1,20 @@
-<?php session_start(); ?>
 
 <!DOCTYPE html>
-<html lang="en"><head>
+<html lang="en" style="
+	background: linear-gradient(-135deg, #f0ffb1, #fdc689);
+"><head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Post</title>
-    <link href="../CSS/postthreadstyles.css" rel="stylesheet" type="text/css">
+    <link href="styles.css" rel="stylesheet" type="text/css">
 </head>
+<body>
+ <?php include('header.php');
 
-
-<body class ="container full-height-grow">
-
-<?php include('header.php');?>
-
-
-<?php
 if (isset($_POST['classi'])){
 
-    $_SESSION['currentClass'] = $_POST['classi'];
-    echo '<strong style="font-size: xx-large">';
+$_SESSION['currentClass'] = $_POST['classi'];
+echo '<strong style="font-size: xx-large">';
     $classid = $_SESSION['currentClass'];
     include ('conn.php');
     $classname = mysqli_query($conn,"select * from classes where id = '$classid'");
@@ -37,69 +33,73 @@ if (isset($_POST['classi'])){
 
 }
 ?>
-
-<div class="row bootstrap snippets">
-		<div class="col-md-6 col-md-offset-2 col-sm-12">
-			<div class="comment-wrapper">
-				<div class="panel panel-info">
-
-					<div class="panel-heading">
-						<p><strong>Comment Panel</strong></p>
-					</div>
-					<div class="panel-body">
-						<textarea class="form-control" placeholder="write a comment..." rows="3" style="margin: 0px; width: 1700px; height: 100px;"></textarea>
-						<br>
-						<button type="button" class="btn btn-info pull-right">Post</button>
-						<div class="clearfix"></div>
-						<hr>
-						<ul class="media-list">
+<div>
+    <form action="" method="post">
+        <textarea input type="text" name="number1" cols="40" rows="5" style="margin-bottom:10px;width:1200px;height: 150px;border: 4px solid #e0b1b1;margin-top: 15px;background-color: white;"></textarea>
 
 
-							<li class="media">
-								<a href="#" class="pull-left">
-									<img src="https://bootdey.com/img/Content/user_1.jpg" alt="" class="img-circle">
-								</a>
-								<div class="media-body">
-									<span class="text-muted pull-right">
-										<small class="text-muted">30 min ago</small>
-									</span>
-									<a>
-									<a href="will be profile link">
-									<strong class="text-success">@JoshGrimm</strong>
-									</a>
-									<p>
-										I loved this course, it was very organized! I believe it is taught by Prof. Hertz <span><a href="#" style="margin-left: 20px;">reply</a></span> 
-									</p>		
-								</div>
-							</li>
 
+        <p><input type="submit"/></p>
+        <ul class="post-OP">
+            <li class="poster">
+					<span class="text-muted pull-right">
+						<small class="text-muted">55 min ago</small>
+					</span>
+                    <strong class="text-success">Posted by: <a>
+                            <a href="will be profile link"> @Rasel Ahmed</strong>
+                    </a>
+            </li>
+        </ul>
+        <hr style="margin-top: 0px;">
+</div>
 
-							<li class="media">
-								<a href="#" class="pull-left">
-									<img src="https://bootdey.com/img/Content/user_2.jpg" alt="" class="img-circle">
-								</a>
-								<div class="media-body">
-									<span class="text-muted pull-right">
-										<small class="text-muted">35 min ago</small>
-									</span>
-									<a>
-									<a href="will be profile link">
-									<strong class="text-success">@MDRafsan</strong>
-								    </a>
-									<p>
-										I found the course very informative and learned a lot.<span><a href="#" style="
-										margin-left: 20px;">reply</a></span>
-									</p>
-								</div>
-							</li>
-
-
-						</ul>
-					</div>
-				</div>
-			</div>
-	
-		</div>
-	</div>
 
 </body></html>
+<?php
+// 1. database credentials
+$host = "tethys.cse.buffalo.edu";
+$username = "mdrafsan";
+$password = "50100208";
+$dbname = "cse442_542_2020_spring_teamg_db";
+// 2. connect to database
+function testdb_connect ($host, $username, $password){
+    $dbh = new PDO("mysql:host=$host;dbname=cse442_542_2020_spring_teamg_db", $username, $password);
+    return $dbh;
+}
+try {
+    $conn = new mysqli($host, $username, $password, $dbname);
+
+    $sql = "SELECT id, subject, content, date FROM POSTS";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th></th><th></th></tr>";
+
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>".$row["subject"]." - "." ".$row["content"]."</td></tr>";
+            $postID = $row["id"];
+            echo "<tr><td>"."<button onclick=\"location.href='/expandedPost.php?post_id=$postID'\" type=\"button\">Go To Post</button>"."<hr>"."</td></tr>";
+
+
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
+
+if (isset($_POST['number1'])) {
+    $dbh = testdb_connect ($host, $username, $password);
+    $description = addslashes ($_POST['number1']);
+    $query = "INSERT INTO POSTS ". "(subject,content, date) "."VALUES ". "('->','$description','2014')";
+
+    $stmt = $dbh->prepare( $query );
+    $product_id=1;
+    $stmt->bindParam(1, $product_id);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "<meta http-equiv='refresh' content='0'>";
+}
+?>
