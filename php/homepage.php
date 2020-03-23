5539dea832f7754c7053f3e0d7e8650c7fbf1c75
Joey Dubill <jwdubill@buffalo.edu>
2020-03-22 22:31:12 -0400

    Added header navigation to homepage Added list of all classes with links to pages #35 Added user classes with links to pages #35 Linked posts to individual classes #37

--- php/homepage.php
+++ php/homepage.php
@@ -18,71 +18,74 @@
 
     <div class = "left-side-wrapper">
 
-        <?php
-        include('conn.php');
-        $user_id = $_SESSION['id'];
+        <strong style="font-size: xx-large"> Your Classes</strong>
 
-
-        //gets all class-user data
-        $allclasses = mysqli_query($conn, "select * from `userclasses` where userid ='$user_id'");
-        if (!$allclasses){
-            echo "Error accessing class list";
-        }
-        //fetches classid list
-        $class_ids = array();
-        while ($line = mysqli_fetch_assoc($allclasses)){
-            $class_ids[] = $line['classid'];
-        }
-        //fetches actual classes by classid and sets to $classes
-        $classes = array();
-        foreach($class_ids as $class_id){
-            $classq = mysqli_fetch_array(mysqli_query($conn, "select * from classes where id ='$class_id'"));
-            $classes[] = $classq;
-        }
-
-
-        //displays the list of a classes as a single form which goes to the class page, with a hidden input for the class ID
-
-
-        echo '<form name = "classf" id = "classform" method = "POST" action = "post-thread.php">
-
+        <form name = "classf" id = "classform" method = "GET" action = "post-thread.php">
                <input name = "classi" type = "hidden" id = "classinput" value = "">
-        
-               <ul class = "classes-list">';
+               <ul class = "classes-list">
+                <?php
+                include('conn.php');
+                $user_id = $_SESSION['id'];
 
-                foreach($classes as $class){
-                    $id = $class['id'];
 
-                    //displays element (one class) with id set as class id
-                    echo '<li onclick="clk(this.id)" id = "'.$id.'" class = "classoption" > '.$class['classnum'] . $class['name'].' </li> ';
+                //gets all class-user data
+                $allclasses = mysqli_query($conn, "select * from `userclasses` where userid ='$user_id'");
+                if (!$allclasses){
+                    echo "Error accessing class list";
+                }
+                //fetches classid list
+                $class_ids = array();
+                while ($line = mysqli_fetch_assoc($allclasses)){
+                    $class_ids[] = $line['classid'];
+                }
+                //fetches actual classes by classid and sets to $classes
+                $classes = array();
+                foreach($class_ids as $class_id){
+                    $classq = mysqli_fetch_array(mysqli_query($conn, "select * from classes where id ='$class_id'"));
+                    $classes[] = $classq;
                 }
 
-        echo '</ul>
-              </form>';
-
-        ?>
+                foreach($classes as $class){
+                         $id = $class['id'];
+                         //displays element (one class) with id set as class id
+                         echo '<li onclick="leftclk(this.id)" id = "'.$id.'" class = "classoption" > '.$class['classnum'] . $class['name'].' </li> ';
+                     } ?>
+               </ul>
+        </form>
 
 
         <script type = "text/javascript">
-            function clk(elem) {
+            function leftclk(elem) {
                 document.getElementById("classinput").setAttribute("value", elem);
                 document.getElementById("classform").submit();
             }
+            function rightclk(elem) {
+                document.getElementById("allclassinput").setAttribute("value", elem);
+                document.getElementById("allclassform").submit();
+            }
         </script>
-
     </div>
 
 
     <div class = "right-side-wrapper">
 
+        <strong style="font-size: xx-large"> Available Classes</strong>
+
+        <form name = "allclassf" id = "allclassform" method = "GET" action = "post-thread.php">
+            <input name = "allclassi" type = "hidden" id = "allclassinput" value = "">
         <ul class = "classes-list">
-            <li > <a href ="post-thread.php" class = "classoption"> Class 1 </a> </li>
-            <li > <a href ="post-thread.php" class = "classoption"> Class 2 </a> </li>
-            <li > <a href ="post-thread.php" class = "classoption"> Class 3 </a> </li>
-            <li > <a href ="post-thread.php" class = "classoption"> Class 4 </a> </li>
+            <?php $class_list = mysqli_query($conn, "select * from classes");
+                    foreach($class_list as $class){
+                        $id = $class['id'];
+                         //displays element (one class) with id set as class id
+                         echo '<li onclick="rightclk(this.id)" id = "'.$id.'" class = "classoption" > '.$class['classnum'] . $class['name'].' </li> ';
+                    } ?>
         </ul>
+        </form>
 
     </div>
+
+    </form>
 
 </section>
 
