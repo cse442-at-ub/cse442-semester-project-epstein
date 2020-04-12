c4ab9fa17382a875f4dfd18fdd1575c0c8df2f12
MD R CHOWDHURY <mdrafsan@buffalo.edu>
2020-04-12 14:25:25 -0400

    Merge reportPost and deleteButton branch

--- php/expandedPost.php
+++ php/expandedPost.php
@@ -2,7 +2,6 @@
 include('header.php');
 if (!isset($_SESSION['id'])) {
     header('Location: loginpage.php');
-
 }
 ?>
 
@@ -38,10 +37,10 @@ if (!isset($_SESSION['id'])) {
                     <img src="../images/IconCSE442.png" alt="Girl in a jacket" style="width:30%;">
                 </center>
                 <span class="login100-form-title">
-						Discussion Board 
+						Discussion Board
                 </span>
                 <div>
-                    
+
                     <p>
                         <?php
 // 1. database credentials
@@ -51,35 +50,152 @@ $password = "50100208";
 $dbname = "cse442_542_2020_spring_teamg_db";
 // 2. connect to database
 try {
-    
+
     $conn = new mysqli($host, $username, $password, $dbname);
     $post_id = $_GET['post_id'];
     $sql = "SELECT id, subject, content, date FROM POSTS where id=$post_id";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         echo "<table><tr><th></th><th></th></tr>";
-        
+
         while($row = $result->fetch_assoc()) {
             echo "<tr><td>"." ".""." ".$row["content"]."</td></tr>";
+            echo "<tr><td>"." ".""." "."Posted On: ".$row["date"]."</td></tr>";
             echo "<tr><td>"."<hr>"."</td></tr>";
-            
+
 
         }
         echo "</table>";
     } else {
         echo "0 results";
     }
-    
+
 } catch(PDOException $e) {
     echo $e->getMessage();
 }
 ?>
                     </p>
                 </div>
-                
+
                 <form action="" method="post">
-                <textarea input type="text" name="number1" cols="40" rows="5" style="margin-bottom:10px;width:700px;height: 150px;border: 4px solid #e0b1b1;margin-top: 15px;background-color: coral;"></textarea>
-		        <p><input type="submit"/></p>
+                <textarea input type="text" name="number1" cols="40" rows="5" style="margin-bottom:1px;margin-left:5px; width:100% ;height: 150px;border: 2px solid black;margin-top: 8px;background-color: #f0ffb1;"></textarea>
+		        <p><input type="submit" style="margin-bottom:10px;margin-left:650px;width:80px;height: 50px;border: 2px solid black;margin-top: 1px;background-color: #fdc689; color:black;"></p>
+
+<?php
+    function testdb_connect ($host, $username, $password){
+        $dbh = new PDO("mysql:host=$host;dbname=cse442_542_2020_spring_teamg_db", $username, $password);
+        return $dbh;
+    }
+    if (isset($_POST['number1'])) {
+        $dbh = testdb_connect ($host, $username, $password);
+        $description = addslashes ($_POST['number1']);
+        if ($description != "") {
+            $postid = $_GET['post_id'];
+            $OP = $_SESSION['id'];
+            $date = date('Y/m/d H:i:s');
+            $query = "INSERT INTO comments ". "(post_id,content,userid,datePosted) "."VALUES ". "('$postid','$description', $OP, '$date')";
+
+            $stmt = $dbh->prepare( $query );
+            $product_id=1;
+            $stmt->bindParam(1, $product_id);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $conn = new mysqli($host, $username, $password, $dbname);
+            $commentID = $_POST['likePressed'];
+            $OP = $_SESSION['id'];
+
+            $sql = "SELECT commentID, userID FROM likesReceived WHERE commentID='$commentID' && userID='$OP'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+
+            } else {
+                $query = "INSERT INTO likesReceived ". "(commentID, userID) "."VALUES ". "('$commentID','$OP')";
+
+
+                $stmt = $dbh->prepare( $query );
+                $product_id=1;
+                $stmt->bindParam(1, $product_id);
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+        }
+    }
+
+    if (isset($_GET['commentToDelete']) && isset($_GET['OPID'])) {
+    $currentUSERID = $_SESSION['id'];
+    $requestUSERID = $_GET['OPID'];
+
+    if ($currentUSERID == $requestUSERID) {
+         $host = "tethys.cse.buffalo.edu";
+         $username = "mdrafsan";
+         $password = "50100208";
+         $dbname = "cse442_542_2020_spring_teamg_db";
+
+         $dbh = testdb_connect ($host, $username, $password);
+
+         $query = "DELETE FROM comments WHERE id=".$_GET["commentToDelete"];
+
+         $stmt = $dbh->prepare( $query );
+         $product_id=1;
+         $stmt->bindParam(1, $product_id);
+         $stmt->execute();
+         $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+
+    try {	
+        $conn = new mysqli($host, $username, $password, $dbname);		
+    $postid = $_GET['post_id'];		
+    $sql = "SELECT post_id, content, userid, datePosted FROM comments WHERE post_id='$postid'";		
+    $result = $conn->query($sql);		
+    if ($result->num_rows > 0) {		
+        $conn = new mysqli($host, $username, $password, $dbname);		
+    $postid = $_GET['post_id'];		
+    $sql = "SELECT id, post_id, content, userid, datePosted FROM comments WHERE post_id='$postid'";		
+    $result = $conn->query($sql);		
+    if ($result->num_rows > 0) {		
+        echo "<table><tr><th></th><th></th></tr>";		
+        		
+        while($row = $result->fetch_assoc()) {		
+            $OPID = $row['userid'];		
+            $commentID = $row["id"];
+            if ($_SESSION['id'] == $row['userid']) {
+                echo "<tr><td>"."<button onclick=\"location.href='expandedPost.php?post_id=$postid&&commentToDelete=$commentID&&OPID=$OPID'\" style=\"border-style: solid; border-radius: 5px;margin-left: 500px; padding-left: 10px; padding-right: 10px;border-color: red;background-color:red; color:white;\"type=\"button\">Delete Comment</button>"."</td></tr>";
+            }
+            $userq = mysqli_query($conn, "select username, name, picture_path from users where id = '$OPID'");
+            $userrows=mysqli_fetch_array($userq);
+            $OPuser = $userrows['username'];
+            $OPpath = $userrows['picture_path'];
+            //display OP
+            echo "<tr><td>Posted by: ";
+            echo $OPuser;
+            echo "<tr><td>Date posted: ";
+            echo $row["datePosted"];
+            //display OP image
+            echo "<img src='".$OPpath."' width='17' height='17' >
+                  <tr></td>";
+            echo "<tr><td>".$row["content"]."</td></tr>";
+            $postID = $row["id"];
+
+            $totalUsers = mysqli_query($conn, "SELECT COUNT(*) FROM `likesReceived` where commentID='$postID'");
+
+            while ($row = $totalUsers->fetch_assoc()) {
+                echo "<tr><td>"."<button name= \"likePressed\" class=\"button-class\" style=\"border-style: solid; border-radius: 5px;margin-right: 10px; padding-left: 10px; padding-right: 10px;border-color: black;background-color:lime; color:black\" value=\"$postID\">Likes </button>".$row['COUNT(*)']."</td></tr>";
+            }
+            echo "<tr><td>".$row["content"]."</td></tr>";
+            $postID = $row["post_id"];
+
+            echo "<tr><td>"."<hr>"."</td></tr>";
+        }
+    }
+    }
+    } catch(PDOException $e) {
+    echo $e->getMessage();
+}
+        echo "</table>";
+?>
 			</div>
 		</div>
 	</div>
@@ -106,57 +222,3 @@ try {
 
 </body>
 </html>
-<?php
-function testdb_connect ($host, $username, $password){
-    $dbh = new PDO("mysql:host=$host;dbname=cse442_542_2020_spring_teamg_db", $username, $password);
-    return $dbh;
-}
-if (isset($_POST['number1'])) {
-    $dbh = testdb_connect ($host, $username, $password);
-    $description = addslashes ($_POST['number1']);
-    $postid = $_GET['post_id'];
-    $OP = $_SESSION['id'];
-    $query = "INSERT INTO comments ". "(post_id,content, userid) "."VALUES ". "('$postid','$description', $OP)";
-    
-    $stmt = $dbh->prepare( $query );
-    $product_id=1;
-    $stmt->bindParam(1, $product_id);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo "<meta http-equiv='refresh' content='0'>";
-}
-    
-try {
-    $conn = new mysqli($host, $username, $password, $dbname);
-    $postid = $_GET['post_id'];
-    $sql = "SELECT post_id, content, userid FROM comments WHERE post_id='$postid'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        echo "<table><tr><th></th><th></th></tr>";
-        
-        while($row = $result->fetch_assoc()) {
-            $OPID = $row['userid'];
-            $userq = mysqli_query($conn, "select username, name, picture_path from users where id = '$OPID'");
-            $userrows=mysqli_fetch_array($userq);
-            $OPuser = $userrows['username'];
-            $OPpath = $userrows['picture_path'];
-
-            //display OP
-            echo "<tr><td>Posted by: ";
-            echo $OPuser;
-            //display OP image
-            echo "<img src='".$OPpath."' width='17' height='17' >
-                  <tr></td>";
-
-            echo "<tr><td>".$row["content"]."</td></tr>";
-            $postID = $row["post_id"];
-            echo "<tr><td>"."<hr>"."</td></tr>";
-        }
-        echo "</table>";
-    } else {
-    }
-    
-} catch(PDOException $e) {
-    echo $e->getMessage();
-}
-?>
--- php/post-thread.php
+++ php/post-thread.php
@@ -16,6 +16,50 @@ if (!isset($_SESSION['id'])) {
 </head>
 <body>
 <?php
+    
+    
+if (isset($_GET['postToDelete']) && isset($_GET['OPID'])) {
+    $currentUSERID = $_SESSION['id'];
+    $requestUSERID = $_GET['OPID'];
+    
+    if ($currentUSERID == $requestUSERID) {
+         $host = "tethys.cse.buffalo.edu";
+         $username = "mdrafsan";
+         $password = "50100208";
+         $dbname = "cse442_542_2020_spring_teamg_db";
+    
+         $dbh = testdb_connect ($host, $username, $password);
+         $classId = $_GET['allclassi'];
+    
+         $query = "DELETE FROM POSTS WHERE id=".$_GET["postToDelete"];
+
+         $stmt = $dbh->prepare( $query );
+         $product_id=1;
+         $stmt->bindParam(1, $product_id);
+         $stmt->execute();
+         $row = $stmt->fetch(PDO::FETCH_ASSOC);                
+    }
+} 
+    
+if (isset($_GET['postToReport'])) {
+    $host = "tethys.cse.buffalo.edu";
+    $username = "mdrafsan";
+    $password = "50100208";
+    $dbname = "cse442_542_2020_spring_teamg_db";
+    
+    $dbh = testdb_connect ($host, $username, $password);
+    $postToReport = addslashes ($_GET['postToReport']);
+    $classId = $_GET['allclassi'];
+    
+    $query = "INSERT INTO reportedPosts ". "(postReported,classId) "."VALUES ". "('$postToReport','$classId' )";
+
+    $stmt = $dbh->prepare( $query );
+    $product_id=1;
+    $stmt->bindParam(1, $product_id);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+}
 if (isset($_GET['classi']) ||  isset($_GET['allclassi'])){
     //to differentiate between all classes and user's classes
     if (!isset($_GET['classi'])){
@@ -30,20 +74,74 @@ if (isset($_GET['classi']) ||  isset($_GET['allclassi'])){
     $namerow = $rows['name'];
     $numrow = $rows['classnum'];
     $classnamefull = $namerow.$numrow;
-
-
+    
     echo '<strong style="font-size: xx-large">';
     echo $classnamefull;
-     echo  '</strong>';
+    echo  '</strong>';
+    $totalPOSTS = 0;
+    $totalUsers = mysqli_query($conn, "SELECT COUNT(*) FROM POSTS where classid = '$classid'");
+    $totalUsers = mysqli_query($conn, "SELECT COUNT(*) FROM `POSTS` where classid = '$classid'");
+        while ($row = $totalUsers->fetch_assoc()) {
+            $totalPOSTS = $row['COUNT(*)'];
+                        
 
+            
+    }
+    
+    $totalStudents = 0;
+    $totalUsers = mysqli_query($conn, "SELECT COUNT(*) FROM userclasses where classid = '$classid'");
+    $totalUsers = mysqli_query($conn, "SELECT COUNT(*) FROM `userclasses` where classid = '$classid'");
+        while ($row = $totalUsers->fetch_assoc()) {
+            $totalStudents = $row['COUNT(*)'];
+                        
+            
+    }
+    
+    echo '<strong style="border: solid 1px; border-color:black; background: white; font-size: normal; color:black; margin: 100px; padding:10px" >';
+            echo "At a glance: Total Posts: ".$totalPOSTS."  "." Total Students: ".$totalStudents;
+                echo  '</strong>';
+    
+     echo '
+<dialog id="favDialog">
+  <form method="dialog">
+    <p><label>Which post would you like to report? :
+      <select>
+        <option></option>';
+    $host = "tethys.cse.buffalo.edu";
+    $username = "mdrafsan";
+    $password = "50100208";
+    $dbname = "cse442_542_2020_spring_teamg_db";
+    $conn = new mysqli($host, $username, $password, $dbname);
+    $sql = "SELECT id, userid, subject, content, date FROM POSTS where classid = '$classid'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+
+        while($row = $result->fetch_assoc()) {
+            echo "<option>".$row["content"]."</option>";
+        }
+    }
+        
+      echo '</select>
+    </label></p>
+    <menu>
+      <button value="cancel">Cancel</button>
+      <button id="confirmBtn" value="default">Confirm</button>
+    </menu>
+  </form>
+</dialog>
+<span>
+
+  <button id="updateDetails" style="width:150px;height:50px; background-color:#ED2939;border:solid 3px; border-color:white; color:white; font-size:15px; font-style:bold" display="inline">Report a Post!</button>
+</span>
+
+<br>
+<br>
+<output aria-live="polite"></output></div>';
 }
 ?>
 <div>
     <form action="" method="post">
         <textarea input type="text" name="number1" cols="40" rows="5" style="margin-bottom:10px;width:1200px;height: 150px;border: 4px solid #e0b1b1;margin-top: 15px;background-color: white;"></textarea>
-
-
-
         <p><input type="submit"/></p>
         <hr style="margin-top: 0px;">
 </div>
@@ -68,15 +166,23 @@ try {
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         echo "<table><tr><th></th><th></th></tr>";
-
         while($row = $result->fetch_assoc()) {
-            echo "<tr><td>".$row["subject"]." - "." ".$row["content"]."</td></tr>";
             $postID = $row["id"];
+            $OPID = $row['userid'];
+            if ($_SESSION['id'] == $row['userid']) {
+                $classId = $_GET['allclassi'];
+
+                echo "<tr><td>"."<button onclick=\"location.href='post-thread.php?allclassi=$classId&&postToDelete=$postID&&OPID=$OPID'\" style=\"border-style: solid; border-radius: 5px;margin-left: 500px; padding-left: 10px; padding-right: 10px;border-color: red;background-color:red; color:white;\"type=\"button\">Delete Post</button>"."</td></tr>";
+            }
+            $postID = $row["id"];
+            
+            echo "<tr><td>".$row["subject"]." - "." ".$row["content"]."</td></tr>";
             $OPID = $row['userid'];
             $userq = mysqli_query($conn, "select username, name, picture_path from users where id = '$OPID'");
             $userrows=mysqli_fetch_array($userq);
             $OPuser = $userrows['username'];
             $OPpath = $userrows['picture_path'];
+            $OPdate = $row['date'];
 
             //display OP
             echo "<tr><td>Posted by: ";
@@ -84,10 +190,14 @@ try {
             //display OP image
             echo "<img src='".$OPpath."' width='17' height='17' >
                   <tr></td>";
+            
+            echo "<tr><td>Posted on: ";
+            echo $OPdate;
 
 
             echo "<tr><td>"."<button onclick=\"location.href='expandedPost.php?post_id=$postID'\" type=\"button\">Go To Post</button>"."<hr>"."</td></tr>";
-
+            
+            
         }
         echo "</table>";
     } else {
@@ -101,15 +211,66 @@ try {
 if (isset($_POST['number1'])) {
     $dbh = testdb_connect ($host, $username, $password);
     $description = addslashes ($_POST['number1']);
-    $OP = $_SESSION['id'];
-    $query = "INSERT INTO POSTS ". "(subject,content, date, classid, userid) "."VALUES ". "('->','$description','2014', $classid, $OP )";
+    
+    if ($description != "") {
+        $OP = $_SESSION['id'];
+        $date = date('Y/m/d H:i:s');
+        $query = "INSERT INTO POSTS ". "(subject,content, date, classid, userid) "."VALUES ". "('->','$description','$date', $classid, $OP )";
 
-    $stmt = $dbh->prepare( $query );
-    $product_id=1;
-    $stmt->bindParam(1, $product_id);
-    $stmt->execute();
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    header("Refresh:0; url=post-thread.php?classi=$classid");
-
+        $stmt = $dbh->prepare( $query );
+        $product_id=1;
+        $stmt->bindParam(1, $product_id);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        echo "<meta http-equiv='refresh' content='0'>";
+        echo $_COOKIE['post'];
+    }
+    
 }
+
+    
 ?>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+
+<script>
+    var updateButton = document.getElementById('updateDetails');
+    var favDialog = document.getElementById('favDialog');
+    var outputBox = document.querySelector('output');
+    var selectEl = document.querySelector('select');
+    var confirmBtn = document.getElementById('confirmBtn');
+
+    // "Update details" button opens the <dialog> modally
+    updateButton.addEventListener('click', function onOpen() {
+      if (typeof favDialog.showModal === "function") {
+        favDialog.showModal();
+      } else {
+        alert("The <dialog> API is not supported by this browser");
+      }
+    });
+    // "Favorite animal" input sets the value of the submit button
+    selectEl.addEventListener('change', function onSelect(e) {
+      confirmBtn.value = selectEl.value;
+    });
+    // "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
+    favDialog.addEventListener('close', function onClose() {
+        if (favDialog.returnValue === "cancel") {
+        } else {
+            outputBox.value = "Your feedback has been sent to the instructor for "+ favDialog.returnValue;
+            document.cookie="post=favDialog.returnValue";
+            var vars = {};
+            var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+                vars[key] = value;
+            });
+            
+            var hello = "hello";
+            var world = "world";
+            window.location.href = "post-thread.php?allclassi=" + vars["allclassi"] + "&postToReport=" + favDialog.returnValue;
+        }
+
+    });
+    
+    
+    
+    
+    
+</script>
